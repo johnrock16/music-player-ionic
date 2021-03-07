9f158a27b0c54a0dc85c50abd0bffab3f4eee9bb
johnrock16 <joaovrocha16@gmail.com>
2021-03-07 07:45:46 -0300

    first commit

---
+++ src/server/index.js
@@ -0,0 +1,104 @@
+
+const express = require('express')
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const fetch = require('node-fetch');
+const dotenv = require('dotenv');
+const path= require('path');
+// const API_NLP='https://api.meaningcloud.com'
+
+const app = express();
+const port = 3000;
+
+dotenv.config();
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const corsOptions = {
+    origin: 'http://localhost:8100',
+    optionsSuccessStatus: 200,
+}
+app.use(cors(corsOptions));
+
+app.use(express.static('dist'))
+
+app.get('/',(req,res)=>{
+    res.sendFile('../../dist/index.html');
+});
+
+app.get('/getPlayList',(req, res)=>{
+    const {id} = req.query;
+    if(id){
+        res.send(PLAYLIST[id])
+        return;
+    }
+})
+
+app.get('/getPlayLists',(req, res)=>{
+    res.send(PLAYLIST)
+})
+
+app.get('/music',(req,res)=>{
+    const {name} = req.query;
+    res.sendFile(path.join(__dirname, '../../assets', name));
+})
+
+app.post('/analizeNews', async (req, res) => {
+    // const {url} = req.body
+    // const urlFetch=`${API_NLP}/sentiment-2.1?${new URLSearchParams({
+    //     key:process.env.API_KEY,
+    //     of:'json',
+    //     url:url,
+    //     lang:'en'
+    // })}`
+    // fetch(urlFetch,{
+    //     method:'POST',
+    // }).then((resolve)=>{
+    //     if(resolve.status===200){
+    //         return resolve.json();
+    //     }
+    //     res.sendStatus(500)
+    // }).then((result)=>{
+    //     res.send(result);
+    // }).catch((error)=>{
+    //     console.log(error);
+    // });
+
+});
+
+app.listen(port, () => {
+    console.log(`Server is running`);
+});
+
+const MUSIC=
+[
+    {
+        index:0,
+        title:"That's What You Get",
+        band:"Paramore",
+        music:"http://localhost:3000/music?name=Paramore-Thats-What-You-Get.mp3",
+        image:"https://images-na.ssl-images-amazon.com/images/I/91wuBxVoekL._AC_SX425_.jpg",
+    },
+    {
+        index:1,
+        title:"Pressure",
+        band:"Paramore",
+        music:"http://localhost:3000/music?name=Paramore-Pressure.mp3",
+        image:"https://i2.rozetka.ua/goods/12577426/96217768_images_12577426294.jpg",
+    }
+]
+
+const PLAYLIST=[
+    {
+        name:'Paramore Riot',
+        isAlbum:true,
+        musics:MUSIC
+    },
+    {
+        name:'Best of Paramore',
+        isAlbum:false,
+        musics:MUSIC
+    }
+]
